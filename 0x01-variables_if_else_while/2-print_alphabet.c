65a0717719aefd06954f1acc212b97da90b295b8
Emmabata <ndinezeemmanuel@gmail.com>
2022-10-14 20:36:55 -0700

    task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+ int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+ for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
@@ -17,11 +17,11 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			putchar(',');
-			putchar(' ');
-		}
+	putchar(',');
+	putchar(' ');
 	}
-	putchar('\n');
-	return (0);
-}
+  }
 
+     putchar('\n');
+ return (0);
+}
